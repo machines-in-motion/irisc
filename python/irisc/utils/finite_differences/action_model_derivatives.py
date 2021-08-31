ece0705fdebe135c55f2fe6829e1744f0d1d9553
Bilal Hammoud <bah436@nyu.edu>
2021-08-31 13:51:11 -0400

    dynamics derivatives for point cliff work, error remains in cost derivatives

--- python/irisc/utils/finite_differences/action_model_derivatives.py
+++ python/irisc/utils/finite_differences/action_model_derivatives.py
@@ -30,43 +30,96 @@ class CostNumDiff:
         dx = np.zeros(self.ndx)
         for i in range(self.ndx):
             dx[i] = DELTA
-            x_new = self.state.integrate(x, dx)
-            self.model.calc(self.data, x_new, u)
+            xnew = self.state.integrate(x, dx)
+            self.model.calc(self.data, xnew, u)
             cost1 = self.data.cost 
-            dx[i] = -DELTA 
-            x_new = self.state.integrate(x, dx)
-            self.model.calc(self.data, x_new, u)
+            xnew = self.state.integrate(x, -dx)
+            self.model.calc(self.data, xnew, u)
             cost2 = self.data.cost 
             self.Lx[i] = cost1 - cost2 
             dx[i] = 0. 
-        self.Lx *= 2./DELTA 
+        self.Lx *= 1./(2.*DELTA) 
 
     def calcLu(self, x, u): 
         du = np.zeros(self.nu)
         for i in range(self.nu):
             du[i] = DELTA
-            u_new = u + du 
-            self.model.calc(self.data, x, u_new)
+            self.model.calc(self.data, x, u+du)
             cost1 = self.data.cost 
-            du[i] = -DELTA
-            u_new = u + du 
-            self.model.calc(self.data, x, u_new)
+            self.model.calc(self.data, x, u-du)
             cost2 = self.data.cost 
             self.Lu[i] = cost1 - cost2 
             du[i] = 0. 
-        self.Lu *= 2./DELTA
+        self.Lu *= 1./(2.*DELTA)
 
     def calcLxx(self, x, u): 
-        dx1 = np.zeros(self.ndx)
-        dx2 = np.zeros(self.ndx)
+        dxi = np.zeros(self.ndx)
+        dxj = np.zeros(self.ndx)
+        for i in range(self.ndx):
+            for j in range(self.ndx): 
+                dxi[i] = DELTA
+                dxj[j] = DELTA
+                xnew = self.state.Integrate(x, dxi+ dxj)
+                self.model.calc(self.data, xnew, u)
+                cost1 = self.data.cost 
+                xnew = self.state.Integrate(x, dxi - dxj)
+                self.model.calc(self.data, xnew, u)
+                cost2 = self.data.cost 
+                xnew = self.state.Integrate(x, -dxi + dxj)
+                self.model.calc(self.data, xnew, u)
+                cost3 = self.data.cost 
+                xnew = self.state.Integrate(x, -dxi - dxj)
+                self.model.calc(self.data, xnew, u)
+                cost4 = self.data.cost 
+                self.Lxx[i,j] = cost1 - cost2 - cost3 + cost4 
+                dxi[i] = 0.
+                dxj[j] = 0.
+        self.Lxx *= 1./(4.*DELTA*DELTA)
 
     def calcLuu(self, x, u): 
-        du1 = np.zeros(self.nu)
-        du2 = np.zeros(self.nu)
+        dui = np.zeros(self.nu)
+        duj = np.zeros(self.nu)
+        for i in range(self.nu):
+            for j in range(self.nu): 
+                dui[i] = DELTA
+                duj[j] = DELTA
+                self.model.calc(self.data, x, u + dui + duj)
+                cost1 = self.data.cost 
+                self.model.calc(self.data, x, u + dui - duj)
+                cost2 = self.data.cost 
+                self.model.calc(self.data, x, u - dui + duj)
+                cost3 = self.data.cost 
+                self.model.calc(self.data, x, u - dui - duj)
+                cost4 = self.data.cost 
+                self.Luu[i,j] = cost1 - cost2 - cost3 + cost4 
+                dui[i] = 0.
+                duj[j] = 0.
+        self.Luu *= 1./(4.*DELTA*DELTA)
 
     def calcLxu(self, x, u): 
         dx = np.zeros(self.ndx) 
         du = np.zeros(self.nu)
+        for i in range(self.ndx): 
+            for j in range(self.nu): 
+                dx[i] = DELTA
+                du[j] = DELTA
+                xnew = self.state.Integrate(x, dx)
+                self.model.calc(self.data, xnew, u+du)
+                cost1 = self.data.cost 
+                xnew = self.state.Integrate(x, dx)
+                self.model.calc(self.data, xnew, u-du)
+                cost2 = self.data.cost 
+                xnew = self.state.Integrate(x, -dx)
+                self.model.calc(self.data, xnew, u-du)
+                cost3 = self.data.cost 
+                xnew = self.state.Integrate(x, -dx)
+                self.model.calc(self.data, xnew, u-du)
+                cost4 = self.data.cost 
+                self.Lxu[i,j] = cost1 - cost2 - cost3 + cost4 
+                dx[i] = 0.
+                du[j] = 0.
+        self.Lxu *= 1./(4*DELTA*DELTA)
+
 
 
 
@@ -84,31 +137,27 @@ class DynamicsNumDiff:
         dx = np.zeros(self.ndx)
         for i in range(self.ndx):
             dx[i] = DELTA
-            x_new = self.state.integrate(x, dx)
-            self.model.calc(self.data, x_new, u)
+            xnew = self.state.integrate(x, dx)
+            self.model.calc(self.data, xnew, u)
             x_next1 = self.data.xnext.copy() 
-            dx[i] = -DELTA 
-            x_new = self.state.integrate(x, dx)
-            self.model.calc(self.data, x_new, u)
+            xnew = self.state.integrate(x, -dx)
+            self.model.calc(self.data, xnew, u)
             x_next2 = self.data.xnext.copy()
             self.Fx[:,i] = self.state.diff(x_next2, x_next1)  
             dx[i] = 0. 
-        self.Fx *= 2./DELTA 
+        self.Fx *= 1./(2.*DELTA)
 
     def calcFu(self, x, u): 
         du = np.zeros(self.nu)
         for i in range(self.nu):
             du[i] = DELTA
-            u_new = u + du 
-            self.model.calc(self.data, x, u_new)
+            self.model.calc(self.data, x, u + du)
             x_next1 = self.data.xnext.copy() 
-            du[i] = -DELTA 
-            u_new = u + du 
-            self.model.calc(self.data, x, u_new)
+            self.model.calc(self.data, x, u - du)
             x_next2 = self.data.xnext.copy()
             self.Fu[:,i] = self.state.diff(x_next2, x_next1)  
             du[i] = 0. 
-        self.Fu *= 2./DELTA 
+        self.Fu *= 1./(2.*DELTA)
 
 
 
