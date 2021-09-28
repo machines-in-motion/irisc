Fixes bug in hopper dynamics, adds rocket dynamics derivatives

python/urisc/utils/action_models/landing_rocket.py
@@ -7,6 +7,7 @@ link: https://ieeexplore.ieee.org/abstract/document/9387078
 
 
 
+from types import DynamicClassAttribute
 import numpy as np 
 import crocoddyl 
 import matplotlib.pyplot as plt 
@@ -34,7 +35,17 @@ class NominalRocket2D:
         """returns df/dx evaluated at x,u """
         dfdx = np.zeros([3,3])
         dfdu = np.zeros([3,2])
-
+        # 
+        dfdx[0,2] = -self.inv_m*u[1]*np.cos(x[2] + u[0]) 
+        dfdx[1,2] = -self.inv_m*u[1]*np.sin(x[2] + u[0])
+        # 
+        b = self.inv_inertia * self.d
+        dfdu[0,0] = -self.inv_m*u[1]*np.cos(x[2] + u[0])
+        dfdu[1,0] = -self.inv_m*u[1]*np.sin(x[2] + u[0])
+        dfdu[2,0] = -b*u[1]*np.cos(u[0])
+        dfdu[0,1] = -self.inv_m*np.sin(x[2] + u[0])
+        dfdu[1,1] = self.inv_m*np.cos(x[2] + u[0])
+        dfdu[2,1] = - b*np.sin(u[0])
         return dfdx, dfdu
 
     def com_transform_3d(self, x, u):
@@ -58,21 +69,46 @@ class DifferentialActionModelRocket2D(crocoddyl.DifferentialActionModelAbstract)
         state =  crocoddyl.StateVector(nx)
         crocoddyl.DifferentialActionModelAbstract.__init__(self, state, nu, ndx)
 
+        self.isTerminal = isTerminal
 
+        self.w = []
+        # running cost only on controls 
+        self.w += [1.e+1]
+        self.w += [1.e+1]
+        # terminal cost is desired state 
 
     def _running_cost(self, t, x, u):
-        pass 
+        cost = self.w[0]*u[0]**2 + self.w[1]*u[1]**2   
+        return cost 
 
-    def _terminal_cost(self, t, x, u):
-        pass 
-
+    def _terminal_cost(self, x):
+        cost = self.w[2]*x[0]**2 # zero x pos 
+        cost += self.w[3]*(x[1]-self.dynamics.d)**2 # y position hover at desired height 
+        cost += self.w[4]*x[2]**2 # zero orientation 
+        cost += self.w[5]*x[3:].dot(x[3:]) # zero velocities for everything 
+        return cost  
 
     def calc(self, data, x, u=None): 
         if u is None:
             u = np.zeros(self.nu)
+        
+        if self.isTerminal:
+            data.cost = self._terminal_cost(x)
+            data.xout = np.zeros(self.state.ndx)
 
 
     def calcDiff(self, data, x, u=None): 
         if u is None:
             u = np.zeros(self.nu)
+
+        
+
+        if self.isTerminal:
+            pass
+        else:
+            Fx, Fu = self.dynamics.derivatives(x,u)
+        
+        data.Fx = Fx.copy()
+        data.Fu = Fu.copy()
+
 

python/urisc/utils/action_models/penumatic_hopper.py
@@ -34,9 +34,9 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
         self.w += [1.e+2] # piston position  w[0]
         self.w += [1.] # control  w[1]
         # jump phase 
-        self.w += [1.e+5] # mass height  w[2]
-        self.w += [1.] # mass velocity w[3] 
-        self.w += [1.e-2] # piston position  w[4]
+        self.w += [1.e+3] # mass height  w[2]
+        self.w += [1.e+3] # mass velocity w[3] 
+        self.w += [1.e-3] # piston position  w[4]
         self.w += [1.] # control weight 
         # terminal 
         self.w += [1.e+2] # piston pos
@@ -64,7 +64,7 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
             data.cost = self._terminal_cost(x) 
             data.xout = np.zeros(2)
         else:
-            self.cost = self._running_cost(self.t, x, u)
+            data.cost = self._running_cost(self.t, x, u)
             e = x[1] - x[0]
             data.xout[1] = u[0] 
             if e < 0.: