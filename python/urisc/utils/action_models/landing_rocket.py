e7927b1519ffed119336c5a25d1dc2deb2b808fb
Bilal Hammoud <bah436@nyu.edu>
2021-09-29 11:03:38 -0400

    hopper code works perfectly, rocket landing bugs fixed, needs cost tuning

--- python/urisc/utils/action_models/landing_rocket.py
+++ python/urisc/utils/action_models/landing_rocket.py
@@ -1,6 +1,6 @@
-""" here we will have two different dynamics 
-Nominal for Optimization 
-Sloch for actual simulation 
+""" A 2 dimensional rocket model with thrust vectoring, 
+rocket is assumed to be rectanglular 
+center of mass is assumed to be right at the center of the rectangle 
 model inspired from "Direct Policy Optimization Using Deterministic Sampling and Collocation" by Howell et. al. 
 link: https://ieeexplore.ieee.org/abstract/document/9387078 
 """
@@ -21,7 +21,7 @@ class NominalRocket2D:
         self.inertia = (1./12.) * self.w * self.l**3 # rectangle inertia around center of mass 
         self.inv_inertia = 1./self.inertia
         self.mass = 100. 
-        self.inv_m = 1./self.m
+        self.inv_m = 1./self.mass
         self.g = 9.81 
 
     def nonlinear_dynamics(self, x, u):
@@ -33,7 +33,7 @@ class NominalRocket2D:
 
     def derivatives(self, x, u):
         """returns df/dx evaluated at x,u """
-        dfdx = np.zeros([3,3])
+        dfdx = np.zeros([3,6])
         dfdu = np.zeros([3,2])
         # 
         dfdx[0,2] = -self.inv_m*u[1]*np.cos(x[2] + u[0]) 
@@ -70,22 +70,37 @@ class DifferentialActionModelRocket2D(crocoddyl.DifferentialActionModelAbstract)
         crocoddyl.DifferentialActionModelAbstract.__init__(self, state, nu, ndx)
 
         self.isTerminal = isTerminal
-
+        self.t = t 
+        self.T = T 
         self.w = []
         # running cost only on controls 
-        self.w += [1.e+1]
-        self.w += [1.e+1]
-        # terminal cost is desired state 
+        self.w += [1.e-3]  # w[0]
+        self.w += [1.e-3]  # w[1]
+        # running cost state weights 
+        self.w += [1.e+3] # x pos  w[2] 
+        self.w += [1.e+3] # y pos  w[3]
+        self.w += [1.e-3] # orientation  w[4] 
+        self.w += [1.e-1] # velcities   w[5]
+        # terminal cost 
+        self.w += [1.e+5] # w[6]
+        self.w += [1.e+5] # w[7]
+        self.w += [1.e+5] # w[8]
+        self.w += [1.e+5] # w[9]
 
     def _running_cost(self, t, x, u):
         cost = self.w[0]*u[0]**2 + self.w[1]*u[1]**2   
-        return cost 
-
-    def _terminal_cost(self, x):
-        cost = self.w[2]*x[0]**2 # zero x pos 
-        cost += self.w[3]*(x[1]-self.dynamics.d)**2 # y position hover at desired height 
+        cost += self.w[2]*x[0]**2 # zero x pos 
+        cost += self.w[3]*x[1]**2 # y position hover at desired height 
         cost += self.w[4]*x[2]**2 # zero orientation 
         cost += self.w[5]*x[3:].dot(x[3:]) # zero velocities for everything 
+        return cost  
+        
+
+    def _terminal_cost(self, x):
+        cost = self.w[6]*x[0]**2 # zero x pos 
+        cost += self.w[7]*x[1]**2 # y position hover at desired height 
+        cost += self.w[8]*x[2]**2 # zero orientation 
+        cost += self.w[9]*x[3:].dot(x[3:]) # zero velocities for everything 
         return cost  
 
     def calc(self, data, x, u=None): 
@@ -95,20 +110,100 @@ class DifferentialActionModelRocket2D(crocoddyl.DifferentialActionModelAbstract)
         if self.isTerminal:
             data.cost = self._terminal_cost(x)
             data.xout = np.zeros(self.state.ndx)
+        else:
+            data.cost = self._running_cost(self.t, x, u)
+            data.xout = self.dynamics.nonlinear_dynamics(x,u)
 
 
     def calcDiff(self, data, x, u=None): 
         if u is None:
             u = np.zeros(self.nu)
-
-        
-
+        # 
+        Fx = np.zeros([self.state.ndx,self.state.ndx]) 
+        Fu = np.zeros([self.state.ndx,self.nu])
+        Lx = np.zeros([self.state.ndx])
+        Lu = np.zeros([self.nu])
+        Lxx = np.zeros([self.state.ndx, self.state.ndx])
+        Luu = np.zeros([self.nu, self.nu])
+        Lxu = np.zeros([self.state.ndx,self.nu])
+        # 
         if self.isTerminal:
-            pass
+            Lx[0] = 2.*self.w[6]*x[0]
+            Lxx[0,0] = 2.*self.w[6]
+            Lx[1] = 2.*self.w[7]*x[1]
+            Lxx[1,1] = 2.*self.w[7]
+            Lx[2] = 2.*self.w[8]*x[2]
+            Lxx[2,2] = 2.*self.w[8]
+            for i in range(3,self.state.ndx):
+                Lx[i] = 2.*self.w[9]*x[i]
+                Lxx[i,i] =  2.*self.w[9]
+
         else:
             Fx, Fu = self.dynamics.derivatives(x,u)
+            Lx[0] = 2.*self.w[2]*x[0]
+            Lxx[0,0] = 2.*self.w[2]
+            Lx[1] = 2.*self.w[3]*x[1]
+            Lxx[1,1] = 2.*self.w[3]
+            Lx[2] = 2.*self.w[4]*x[2]
+            Lxx[2,2] = 2.*self.w[4]
+            for i in range(3,self.state.ndx):
+                Lx[i] = 2.*self.w[5]*x[i]
+                Lxx[i,i] =  2.*self.w[5]
+            Lu[0] = 2.*self.w[0]*u[0]
+            Lu[1] = 2.*self.w[1]*u[1]
+            Luu[0,0] = 2.*self.w[0]
+            Luu[1,1] = 2.*self.w[1]
         
         data.Fx = Fx.copy()
         data.Fu = Fu.copy()
+        data.Lx = Lx.copy()
+        data.Lu = Lu.copy()
+        data.Lxx = Lxx.copy()
+        data.Luu = Luu.copy()
+        data.Lxu = Lxu.copy()
+
+
+
+
+
+
+if __name__=="__main__": 
+
+    x0 = np.array([10., 20., np.pi/6, 0., 0., 0.]) 
+    MAX_ITER = 1000
+    T = 300 
+    dt = 1.e-2
+    running_models = []
+    for t in range(T): 
+        diff_rocket = DifferentialActionModelRocket2D(t, T, False) 
+        running_models += [crocoddyl.IntegratedActionModelEuler(diff_rocket, dt)] 
+    diff_rocket = DifferentialActionModelRocket2D(T, T, True) 
+    terminal_model = crocoddyl.IntegratedActionModelEuler(diff_rocket, dt) 
+
+    problem = crocoddyl.ShootingProblem(x0, running_models, terminal_model)
+    fddp = crocoddyl.SolverFDDP(problem)
+    fddp.setCallbacks([
+    crocoddyl.CallbackLogger(),
+    crocoddyl.CallbackVerbose()
+    ])
+
+    xs = [x0]*(T+1)
+    us = [np.zeros(2)]*T
+
+    converged = fddp.solve(xs,us, MAX_ITER)
+    if not converged:
+        print("Solver Did Not Converge !!")
+
+    time_array = dt*np.arange(T+1)
+
+    plt.figure("trajectory plot")
+    plt.plot(np.array(fddp.xs)[:,0],np.array(fddp.xs)[:,1], label="Rocket Position")
+
+
+    plt.figure("rocket orientation")
+    plt.plot(time_array,np.array(fddp.xs)[:,2], label="Orientation")
+
+    plt.show()
+
 
 
--- python/urisc/utils/action_models/penumatic_hopper.py
+++ python/urisc/utils/action_models/penumatic_hopper.py
@@ -6,7 +6,52 @@ import crocoddyl
 import matplotlib.pyplot as plt 
 LINE_WIDTH = 100 
 
-we = .05#001 # exponential time weight 
+
+class PenumaticHopped1D:
+    def __init__(self):
+        self.g = 9.81 
+        self.mass = 1. 
+        self.k = 500. 
+        self.alpha = 1.e-2 
+        self.inv_m = 1./self.mass 
+
+    def nonlinear_dynamics(self, x, u):
+        acc = np.zeros(2)
+        e = x[1] - x[0]
+        acc[1] = u[0] 
+        if e < 0.:
+            acc[0] = -self.g
+        elif e >= 0 and e < self.alpha:
+            scale = .5*self.k / self.alpha
+            acc[0] = self.inv_m*scale*(e**2)  - self.g
+        else:
+            acc[0] = self.inv_m*self.k*e - .5*self.inv_m*self.k*self.alpha  - self.g
+        return acc
+
+    def derivatives(self, x, u):
+        """returns df/dx evaluated at x,u """
+        dfdx = np.zeros([2,4])
+        dfdu = np.zeros([2,1])
+        # 
+        e = x[1] - x[0]
+        if e<0.:
+            pass                  
+        elif e >= 0. and e < self.alpha:
+            dfdx[0,0] = -self.inv_m*(self.k/self.alpha)*e 
+            dfdx[0,1] = self.inv_m*(self.k/self.alpha)*e 
+        else:
+            dfdx[0,0] = -self.inv_m*self.k 
+            dfdx[0,1] = self.inv_m*self.k
+        # 
+        dfdu[1,0] = 1. 
+        return dfdx, dfdu
+
+
+
+
+
+
+
 
 class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
     def __init__(self, t, T, isTerminal=False):
@@ -17,12 +62,13 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
         nu = 1 
         state =  crocoddyl.StateVector(nx)
         crocoddyl.DifferentialActionModelAbstract.__init__(self, state, nu, ndx)
-        self.g = - 9.81 
+        self.dynamics = PenumaticHopped1D()
+        # self.g = - 9.81 
         self.isTerminal = isTerminal
-        self.mass = 1. 
-        self.k = 500. 
-        self.alpha = .1
-        self.z_des = 20.
+        # self.mass = 1. 
+        # self.k = 500. 
+        # self.alpha = .01
+        self.z_des = 3.
         self.scale = .5/T  
         #
         self.t = t  # will be used to scale costs 
@@ -32,16 +78,17 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
         self.w = [] 
         # running cost weights 
         self.w += [1.e+2] # piston position  w[0]
-        self.w += [1.] # control  w[1]
+        self.w += [1.e+0] # control  w[1]
         # jump phase 
         self.w += [1.e+3] # mass height  w[2]
         self.w += [1.e+3] # mass velocity w[3] 
-        self.w += [1.e-3] # piston position  w[4]
-        self.w += [1.] # control weight 
+        self.w += [1.e+1] # piston position  w[4]
+        self.w += [1.e+1] # control weight 
         # terminal 
-        self.w += [1.e+2] # piston pos
+        self.w += [1.e+2] # mass position 
+        self.w += [1.e+2] # piston position 
+        self.w += [1.e+2] # mass and piston velocties 
         
-
 
     def _running_cost(self, t, x, u): 
         if t<= self.t1 or t > self.t2:
@@ -52,8 +99,7 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
         return cost 
     
     def _terminal_cost(self, x):
-        # cost = .5*self.w[6]*(x[0]-self.z_des)**2 + self.w[7]*x[2]**2 + self.w[8]*x[1]**2
-        cost = self.w[6]*x[1]**2 
+        cost = self.w[6]*x[0]**2 + self.w[7]*x[1]**2 + self.w[8]*x[2:].dot(x[2:])
         return cost 
 
     def calc(self, data, x, u=None): 
@@ -65,15 +111,8 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
             data.xout = np.zeros(2)
         else:
             data.cost = self._running_cost(self.t, x, u)
-            e = x[1] - x[0]
-            data.xout[1] = u[0] 
-            if e < 0.:
-                data.xout[0] = self.g
-            elif e >= 0 and e < self.alpha:
-                scale = .5*self.k / self.alpha
-                data.xout[0] = scale*(e**2)  + self.g
-            else:
-                data.xout[0] = self.k*e - .5*self.k*self.alpha  + self.g
+            data.xout[:] = self.dynamics.nonlinear_dynamics(x,u)
+
 
     def calcDiff(self, data, x, u=None): 
         if u is None:
@@ -88,14 +127,15 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
         Lxu = np.zeros([4,1])
         # COST DERIVATIVES 
         if self.isTerminal:
-            # Lx[0] = self.w[6]*(x[0]-self.z_des)
-            # Lxx[0,0] = self.w[6]  
-            Lx[1] = 2.*self.w[6]*x[1]
-            Lxx[1,1] = 2.*self.w[6]
-            # Lx[2] = 2.*self.w[7]*x[2]
-            # Lxx[2,2] = 2.*self.w[7]
+            Lx[0] = 2.*self.w[6]*x[0]
+            Lxx[0,0] = 2.*self.w[6]  
+            Lx[1] = 2.*self.w[7]*x[1]
+            Lxx[1,1] = 2.*self.w[7]
+            Lx[2] = 2.*self.w[8]*x[2]
+            Lxx[2,2] = 2.*self.w[8]
+            Lx[3] = 2.*self.w[8]*x[3]
+            Lxx[3,3] = 2.*self.w[8]
         else:
-            Fu[1,0] = 1.  
             t = self.t 
             if t<= self.t1 or t > self.t2:
                 Lu[0] = 2*self.scale*self.w[1]*u[0]
@@ -111,21 +151,8 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
                 Lxx[1,1] = 2.*self.w[4]
                 Lx[2] = 2.*self.w[3]*x[2] 
                 Lxx[2,2] = 2.*self.w[3]
-
-            
-                
-
-
-            # DYNAMICS DERIVATIVES  
-            e = x[1] - x[0]
-            if e<0.:
-                pass                  
-            elif e >= 0. and e < self.alpha:
-                Fx[0,0] = -(self.k/self.alpha)*e 
-                Fx[0,1] = (self.k/self.alpha)*e 
-            else:
-                Fx[0,0] = -self.k 
-                Fx[0,1] = self.k
+            # dynamics derivatives 
+            Fx, Fu = self.dynamics.derivatives(x,u)
         # COPY TO DATA 
         data.Fx = Fx.copy()
         data.Fu = Fu.copy()
@@ -139,14 +166,10 @@ class DifferentialActionModelHopper(crocoddyl.DifferentialActionModelAbstract):
 
 
 if __name__ =="__main__":
-    # print(" Testing Penumatic Hopper with DDP ".center(LINE_WIDTH, '#'))
-    # hopper_diff_running =  DifferentialActionModelHopper()
-    # hopper_diff_terminal = DifferentialActionModelHopper(isTerminal=True)
-    # print(" Constructing differential models completed ".center(LINE_WIDTH, '-'))
-
+    print(" Testing Penumatic Hopper with DDP ".center(LINE_WIDTH, '#'))
     x0 = np.array([0., 0., 0., 0.])
     MAX_ITER = 1000
-    T = 200 
+    T = 300 
     dt = 1.e-2
     running_models = []
     for t in range(T): 
@@ -154,21 +177,19 @@ if __name__ =="__main__":
         running_models += [crocoddyl.IntegratedActionModelEuler(diff_hopper, dt)] 
     diff_hopper = DifferentialActionModelHopper(T, T, True) 
     terminal_model = crocoddyl.IntegratedActionModelEuler(diff_hopper, dt) 
-    # hopper_running = crocoddyl.IntegratedActionModelEuler(hopper_diff_running, dt) 
-    # hopper_terminal = crocoddyl.IntegratedActionModelEuler(hopper_diff_terminal, dt) 
-    print(" Constructing integrated models completed ".center(LINE_WIDTH, '-'))
+    # print(" Constructing integrated models completed ".center(LINE_WIDTH, '-'))
 
     problem = crocoddyl.ShootingProblem(x0, running_models, terminal_model)
-    print(" Constructing shooting problem completed ".center(LINE_WIDTH, '-'))
+    # print(" Constructing shooting problem completed ".center(LINE_WIDTH, '-'))
     
     fddp = crocoddyl.SolverFDDP(problem)
-    print(" Constructing FDDP solver completed ".center(LINE_WIDTH, '-'))
+    # print(" Constructing FDDP solver completed ".center(LINE_WIDTH, '-'))
     fddp.setCallbacks([
     crocoddyl.CallbackLogger(),
     crocoddyl.CallbackVerbose()
     ])
     xs = [x0]*(T+1)
-    us = [np.array([1.e-3])]*T
+    us = [np.array([0.])]*T
 
     converged = fddp.solve(xs,us, MAX_ITER)
 
